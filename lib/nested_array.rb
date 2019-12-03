e5906efdaed65c3543f661987a8bd17d830545d9
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-03 02:22:07 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,11 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-  [
+  assembled_matrix = [
+  #0  
+  ["Don Gately", "Joelle van Dyne", "Pat M.","Kate Gompert", "Bruce Green"]  
+  #1
+  ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
     ]
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
